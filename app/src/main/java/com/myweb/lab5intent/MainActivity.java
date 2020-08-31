f4b462fc8fafbca4ac37ab80ae3d3d82265ee89c
naratthaa <tanarat@github.com>
2020-08-31 16:25:29 +0700

    Lab5_Sec1_613020892-2

---
+++ app/src/main/java/com/myweb/lab5intent/MainActivity.java
@@ -0,0 +1,9 @@
+package com.myweb.lab5intent;
+
+import android.app.Activity;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    public void onClickShowDetail(View view) {
+    }
+}
